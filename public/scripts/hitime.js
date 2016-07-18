1ea0d8715f28f7800344af94e72e68a9acff9ff8
Sripathi Krishnan <Sripathi.Krishnan@gmail.com>
2016-07-18 17:39:36 +0530

    Stress testing - moved to production react.js

--- public/scripts/hitime.js
+++ public/scripts/hitime.js
@@ -10,11 +10,53 @@ var SpreadSheetContainer = React.createClass({
   getInitialState: function() {
     return {
         "data": [
-          [1, "HashedIn", "Sales", 90, "Weekly Sales Meeting"],
-          [2, "Technology", "Training", 30, "Handling forms with React & Redux"],
-          [3, "Jican-Sales", "Review", 10, "Presentation Review"]
+          [1, "HashedIn", "Sales", 90, "Weekly Sales Meeting", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [2, "Technology", "Training", 30, "Handling forms with React & Redux", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
+          [3, "Jican-Sales", "Review", 10, "Presentation Review", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"]
         ],
-        "headerRow": ["#", "Code", "Type", "Time Spent", "Description"],
+        "headerRow": ["#", "Code", "Type", "Time Spent", "Description", "dog", "cat", "elephant", "turkey", "monkey", "leopard", "lion", "tiger", "~!@#$%^&*()_+=;':,.<>/?", "brinjal", "aubergine", 4343.32, "whiteboard", "tv", "video", "audio"],
         "selectedCell": {
           "row": 1,
           "col": 2,
@@ -34,6 +76,11 @@ var SpreadSheetContainer = React.createClass({
   moveDown: function() {
     this.moveVertically(1);
   },
+  addRows: function(numRows) {
+    var data = this.state.data;
+    data.push(["", "", "", "", ""]);
+    this.setState({"data": data});
+  },
   constrainHorizontally: function(pos) {
     var newPos;
     var numColumns = this.state.headerRow.length;
@@ -52,7 +99,8 @@ var SpreadSheetContainer = React.createClass({
     var newPos;
     var numRows = this.state.data.length;
     if (pos > numRows - 1) {
-      newPos = numRows - 1;
+      this.addRows(pos - numRows + 1);
+      newPos = pos;
     }
     else if (pos < 0) {
       newPos = 0;
@@ -75,33 +123,43 @@ var SpreadSheetContainer = React.createClass({
     this.setState({"selectedCell": newSelectedCell});
   },
   handleKeyDown: function(selectedCell, e) {
-    if (!this.state.selectedCell.isEditMode) {
-      if (e.key === 'ArrowRight') {
+    if (e.key === 'ArrowRight') {
+      if (!this.state.selectedCell.isEditMode) {
         this.moveRight();
-        e.preventDefault();
-      } else if (e.key === 'ArrowLeft') {
-        this.moveLeft();
-        e.preventDefault();
-      } else if (e.key === 'ArrowUp') {
-        this.moveUp();
-        e.preventDefault();
-      } else if (e.key === 'ArrowDown') {
-        this.moveDown();
         e.preventDefault();
       }
     }
-    if (e.key === 'Escape') {
+    else if (e.key === 'ArrowLeft') {
+      if (!this.state.selectedCell.isEditMode) {
+        this.moveLeft();
+        e.preventDefault();
+      }
+    }
+    else if (e.key === 'ArrowUp') {
+      this.moveUp();
+      e.preventDefault();
+    }
+    else if (e.key === 'ArrowDown') {
+      this.moveDown();
+      e.preventDefault();
+    }
+    else if (e.key === 'Escape') {
       var selectedCell = Object.assign({}, this.state.selectedCell, {"isEditMode": false});
       this.setState({"selectedCell": selectedCell});
       e.preventDefault();
     }
     //enter
     else if (e.keyCode === 13) {
-      var selectedCell = Object.assign({}, this.state.selectedCell, {"isEditMode": true});
-      this.setState({"selectedCell": selectedCell});
-      e.preventDefault();
+      if (!this.state.selectedCell.isEditMode) {
+        var selectedCell = Object.assign({}, this.state.selectedCell, {"isEditMode": true});
+        this.setState({"selectedCell": selectedCell});
+        e.preventDefault();
+      }
+      else {
+        this.moveDown();
+      }
     }
-    //enter
+    //tab
     else if (e.keyCode === 9) {
       if (e.shiftKey) {
         this.moveLeft();
@@ -110,6 +168,10 @@ var SpreadSheetContainer = React.createClass({
        this.moveRight(); 
       }
       e.preventDefault();
+    }
+    else {
+      var selectedCell = Object.assign({}, this.state.selectedCell, {"isEditMode": true});
+      this.setState({"selectedCell": selectedCell});
     }
   },
   handleClick: function(clickedCell, e) {
@@ -121,6 +183,12 @@ var SpreadSheetContainer = React.createClass({
     var selectedCell = Object.assign({}, this.state.selectedCell, cell);
     this.setState({"selectedCell": selectedCell});
   },
+  handleOnChange: function(cell, e) {
+    /*TODO: okay to make inplace changes?*/
+    var data = this.state.data;
+    data[cell.row][cell.col] = e.target.value;
+    this.setState({"data": data});
+  },
   render: function() {
     return (
         <SpreadSheet data={this.state.data} 
@@ -128,7 +196,8 @@ var SpreadSheetContainer = React.createClass({
           selectedCell={this.state.selectedCell}
           onKeyDown={this.handleKeyDown}
           handleClick={this.handleClick}
-          handleOnFocus={this.handleOnFocus}/>
+          handleOnFocus={this.handleOnFocus}
+          handleOnChange={this.handleOnChange}/>
     )
   }
 })
@@ -152,7 +221,8 @@ var SpreadSheet = React.createClass({
                     selectedCell={props.selectedCell}
                     onKeyDown={props.onKeyDown}
                     handleClick={props.handleClick}
-                    handleOnFocus={props.handleOnFocus}/>
+                    handleOnFocus={props.handleOnFocus}
+                    handleOnChange={props.handleOnChange}/>
           })}
         </tbody>
       </table>
@@ -171,7 +241,8 @@ var Row = React.createClass({
                   selectedCell={props.selectedCell}
                   onKeyDown={props.onKeyDown}
                   handleClick={props.handleClick}
-                  handleOnFocus={props.handleOnFocus}/>
+                  handleOnFocus={props.handleOnFocus}
+                  handleOnChange={props.handleOnChange}/>
         })}
       </tr>
     )
@@ -190,6 +261,10 @@ var Cell = React.createClass({
   handleOnFocus: function(e) {
     var me = {"row": this.props.rownum, "col": this.props.colnum};
     this.props.handleOnFocus(me, e);
+  },
+  handleOnChange: function(e) {
+    var me = {"row": this.props.rownum, "col": this.props.colnum};
+    this.props.handleOnChange(me, e);
   },
   render: function() {
     var props = this.props;
@@ -211,6 +286,7 @@ var Cell = React.createClass({
                 self.focus();
               }
             }}
+          onChange={this.handleOnChange}
           value={this.props.data}/>
       </td>
       )
